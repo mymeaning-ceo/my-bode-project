d252ed9a75fb65324f89c02feed53cf6c33fca32
mymeaning <ceo@mymeaning.co.kr>
2025-06-12 19:08:33 +0900

    list 수정안돰

--- routes/logout.js
+++
@@ -1,9 +0,0 @@
-app.get('/logout', (req, res, next) => {
-    req.logout(err => {
-      if (err) return next(err);
-      req.session.destroy(() => {
-        res.redirect('/login');  // 또는 홈으로 이동
-      });
-    });
-  });
-  
