6f63589fb2eb672f1a0ffe6bb7e0e2984509cb93
mymeaning-ceo <ceo@mymeaning.co.kr>
2025-06-13 15:40:37 +0900

    Patch modules for Node 22

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,32 +1,15 @@
-const passport = require('passport');
-const { ObjectId } = require('mongodb');
-
-const user = await db.collection('users').findOne({ _id: new ObjectId(id) });
-
 function checkLogin(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
-  } else {
-    res.redirect('/login');
   }
+  res.redirect('/login');
 }
 
 function checkAdmin(req, res, next) {
   if (req.isAuthenticated() && req.user?.username === 'admin') {
     return next();
-  } else {
-    res.status(403).send('관리자만 접근 가능합니다.');
   }
+  res.status(403).send('관리자만 접근 가능합니다.');
 }
-// 예시: 로그인 시 세션에 저장하는 코드
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  const user = await db.collection('users').findOne({ _id: new ObjectId(id) });
-  done(null, user); // ← 여기에서 req.user에 들어감
-});
-
 
 module.exports = { checkLogin, checkAdmin };
