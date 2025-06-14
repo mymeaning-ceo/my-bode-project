b278395822e4237912107dea4c5b337d1313372f
mymeaning <ceo@mymeaning.co.kr>
2025-06-14 18:57:57 +0900

    test: mock multer to bypass upload middleware

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,17 @@
-// jest.setup.js
-// upload.js 모킹: S3 설정을 요구하지 않도록 차단
+// upload.js 모킹 (이미 추가된 부분)
 jest.mock("./upload", () => ({
-    ensureBucket: jest.fn().mockResolvedValue(), // 필요한 최소 인터페이스
-  }));
+  ensureBucket: jest.fn().mockResolvedValue(),
+}));
+
+// multer 모킹 추가
+jest.mock("multer", () => {
+  const mockMulter = () => ({
+    single: () => (req, res, next) => next(),
+    array: () => (req, res, next) => next(),
+    fields: () => (req, res, next) => next(),
+  });
+  mockMulter.single = () => (req, res, next) => next();
+  mockMulter.array = () => (req, res, next) => next();
+  mockMulter.fields = () => (req, res, next) => next();
+  return mockMulter;
+});
