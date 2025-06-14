98798b5741ae1b657b0f1fdc19f9015ec2c74bb5
mymeaning <ceo@mymeaning.co.kr>
2025-06-14 13:39:32 +0900

    fix: remove EOF and correct asyncHandler

--- middlewares/asyncHandler.js
+++ middlewares/asyncHandler.js
@@ -1,4 +1,1 @@
-module.exports = (fn) => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-  EOF
+module.exports = (fn) => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
