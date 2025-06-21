b611011970205dc4cacb703cd496776035419f02
mymeaning <ceo@mymeaning.co.kr>
2025-06-21 15:36:29 +0900

    admin.js 기능 수정 및 업데이트

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,18 @@
-// jest.config.js
 module.exports = {
+    // Node.js 백엔드라면:
     testEnvironment: "node",
-    detectOpenHandles: true,
-    forceExit: true,
-    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-};
+  
+    // 프론트엔드(React 등)라면:
+    // testEnvironment: "jsdom",
+  
+    // 테스트 파일 위치 패턴
+    testMatch: ["**/tests/**/*.test.js"],
+  
+    // 커버리지 옵션
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    collectCoverageFrom: [
+      "src/**/*.js",
+      "!src/**/index.js" // 제외할 파일
+    ]
+  };
