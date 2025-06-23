131d91bf25f14da1d0166afe27a921b309d5d808
mymeaning-ceo <ceo@mymeaning.co.kr>
2025-06-23 20:15:18 +0900

    feat: improve search with fuzzy highlight

---
+++ services/searchService.js
@@ -0,0 +1,60 @@
+const escapeHtml = (str = '') =>
+  str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
+
+const escapeRegExp = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+function highlight(text = '', term = '') {
+  const regex = new RegExp(escapeRegExp(term), 'gi');
+  return escapeHtml(text).replace(regex, (m) => `<mark>${m}</mark>`);
+}
+
+async function searchPosts(db, term, page = 1, limit = 5) {
+  const skip = (page - 1) * limit;
+  const pipeline = [
+    {
+      $search: {
+        index: 'title_index',
+        text: {
+          query: term,
+          path: ['title', 'content'],
+          fuzzy: { maxEdits: 1 },
+        },
+      },
+    },
+    { $skip: skip },
+    { $limit: limit },
+  ];
+  const countPipeline = [
+    {
+      $search: {
+        index: 'title_index',
+        text: {
+          query: term,
+          path: ['title', 'content'],
+        },
+      },
+    },
+    { $count: 'total' },
+  ];
+
+  const [docs, countRes] = await Promise.all([
+    db.collection('post').aggregate(pipeline).toArray(),
+    db.collection('post').aggregate(countPipeline).toArray(),
+  ]);
+  const total = countRes[0]?.total || 0;
+  const totalPage = Math.ceil(total / limit);
+
+  docs.forEach((d) => {
+    d.titleHighlighted = highlight(d.title, term);
+    d.contentHighlighted = highlight(d.content, term);
+  });
+
+  return { docs, totalPage, totalCount: total };
+}
+
+module.exports = { searchPosts };
