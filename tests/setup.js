6be9f7e787c6492d1636c1dba2fc9c0bc3137faf
mymeaning-ceo <ceo@mymeaning.co.kr>
2025-06-24 21:46:40 -0700

    Create setup.js

---
+++ tests/setup.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+let mongo;
+
+beforeAll(async () => {
+  mongo = await MongoMemoryServer.create();
+  const uri = mongo.getUri();
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  if (mongo) await mongo.stop();
+});
+
+afterEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
