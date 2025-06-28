6174384631ad313a2738a2e52f5c830032350494
mymeaning-ceo <ceo@mymeaning.co.kr>
2025-06-28 19:30:22 +0900

    test: wrap Login in router

--- client/src/pages/Login.test.js
+++ client/src/pages/Login.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Login from './Login';
 
 test('renders login form', () => {
-  render(<Login />);
+  render(
+    <MemoryRouter>
+      <Login />
+    </MemoryRouter>
+  );
   expect(screen.getByRole('heading', { name: '로그인' })).toBeInTheDocument();
   expect(screen.getByLabelText('아이디')).toBeInTheDocument();
   expect(screen.getByLabelText('비밀번호')).toBeInTheDocument();
