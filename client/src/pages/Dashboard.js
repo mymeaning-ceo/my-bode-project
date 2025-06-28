7bd7600db9b81d165d1e141750f8f826a828e95a
mymeaning-ceo <ceo@mymeaning.co.kr>
2025-06-28 18:55:33 +0900

    feat: add React dashboard layout with sidebar

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>대시보드</h1>
+      <p>환영합니다. 원하는 메뉴를 선택하세요.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
