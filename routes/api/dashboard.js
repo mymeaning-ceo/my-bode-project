cc3e87a6eaf7195cf83e8e6f180d9806fe01f263
mymeaning-ceo <ceo@mymeaning.co.kr>
2025-07-05 14:26:33 +0900

    refactor(dashboard): use controller for ad cost

--- routes/api/dashboard.js
+++ routes/api/dashboard.js
@@ -2,38 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../../controllers/dashboardController');
 
-router.get('/ad-cost-daily', async (req, res) => {
-  const db = req.app.locals.db;
-
-  try {
-    const data = await db
-      .collection('adHistory')
-      .aggregate([
-        {
-          $group: {
-            _id: '$date',
-            totalCost: { $sum: '$cost' },
-          },
-        },
-        { $sort: { _id: 1 } },
-        {
-          $project: {
-            _id: 0,
-            date: '$_id',
-            totalCost: 1,
-          },
-        },
-      ])
-      .toArray();
-
-    res.json(data);
-  } catch (err) {
-    console.error(err);
-    res
-      .status(500)
-      .json({ message: '서버에서 데이터를 조회하는 중 오류가 발생했습니다.' });
-  }
-});
+router.get('/ad-cost-daily', ctrl.getDailyAdCost);
 
 router.get('/city-temp', ctrl.getCityTempHistory);
 router.post('/city-temp', ctrl.saveCityTemp);
